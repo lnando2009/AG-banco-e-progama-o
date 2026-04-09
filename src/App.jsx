edcb2d99b434c5bb1d429068667440adbefbe4ca
lnando2009 <41671@raphaeldisanto.com.br>
2026-04-09 11:46:30 -0300

    Add files via upload

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { useState } from 'react'
 import './App.css'
 import logoGta from './assets/logoPostgres.svg'
-import { Joystick, Gamepad2, GamepadDirectional } from 'lucide-react';
+import { Apple } from 'lucide-react';
 
 export default function App() {
 
@@ -13,11 +13,24 @@ export default function App() {
         </section>
 
         <footer>
-          <small>@2026 ag2cti.com</small>
-          <div id="social">
+          <div id="social" >
+            <h1> Coisas Boas</h1>
+            <p> O PostgreSQL é um banco de dados confiável, rápido e gratuito, com suporte a consultas complexas e grande variedade de tipos de dados. Ele se destaca pela segurança, estabilidade e capacidade de lidar com aplicações grandes, além de ser altamente extensível e mantido por uma comunidade ativa.
+            </p>
+
           
           </div>
+          <div id="social">
+            <h1> Como Funciona</h1>
+            <p> O PostgreSQL armazena dados em tabelas e executa comandos SQL enviados pela aplicação. Ele processa essas consultas de forma otimizada, garante que os dados permaneçam corretos e seguros, e usa memória e índices para responder rapidamente.</p>
+          </div>
+          <div id="social">
+            <h1> Quem Usa</h1>
+            <p> O PostgreSQL é usado por muitas empresas e organizações grandes que precisam de um banco de dados confiável e escalável, como Netflix, Spotify, Instagram e Apple, além de governos, universidades e startups em geral.</p>
+            <Apple id='maca'/>
+            </div>
         </footer>
+
       </main>
     </>
   )
